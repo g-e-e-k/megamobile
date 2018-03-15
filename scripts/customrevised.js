d60b1d15afa9d3bb5a1c8d8e25c25527792b2136
g-e-e-k-o-f-f-i-c-e <geekdheeraj@icloud.com>
2018-03-15 18:13:39 +0530

    logins updated

---
+++ scripts/customrevised.js
@@ -0,0 +1,13 @@
+﻿//function adjustHeaderSection()
+//{
+//    setTimeout(function () {
+//        $('.page-fullscreen-content').css('top', '-45px');
+//        $('.page-fullscreen-content').css('margin-top', 'auto');
+//    }, 50);
+
+//}
+
+//$(document).ready(function () {
+//    adjustHeaderSection();
+//    window.addEventListener("orientationchange", function () { adjustHeaderSection() }, false);
+//});
