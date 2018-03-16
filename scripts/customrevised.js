6e9ba66896bbf0e743bbc9c8d0a8037f943e650a
g-e-e-k-o-f-f-i-c-e <geekdheeraj@icloud.com>
2018-03-16 16:57:42 +0530

    first apk conversion

--- scripts/customrevised.js
+++ scripts/customrevised.js
@@ -1,13 +1,11 @@
-﻿//function adjustHeaderSection()
-//{
-//    setTimeout(function () {
-//        $('.page-fullscreen-content').css('top', '-45px');
-//        $('.page-fullscreen-content').css('margin-top', 'auto');
-//    }, 50);
+﻿function adjustHeaderSection()
+{
+    setTimeout(function () {
+        var applyHeight = $('#page-content').outerHeight() - 55;
+        $('#page-content').css('min-height', applyHeight);
+    }, 50);
+}
 
-//}
-
-//$(document).ready(function () {
-//    adjustHeaderSection();
-//    window.addEventListener("orientationchange", function () { adjustHeaderSection() }, false);
-//});
+$(document).ready(function () {
+    adjustHeaderSection();
+});
